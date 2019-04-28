f881388e5d7c979191b540ad16f5e8b23966da01
palewire <ben.welsh@gmail.com>
2019-04-28 11:35:39 -0700

    Download the image

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,7 @@
+import requests
 import collections
+from PIL import Image
+from io import BytesIO
 from django.db import models
 
 
@@ -47,3 +50,11 @@ class Photo(models.Model):
         Returns the URL where a JPG file can be downloaded.
         """
         return f"https://tessa.lapl.org/utils/getdownloaditem/collection/photos/id/{self.lapl_id}"
+
+    def get_image(self):
+        """
+        Downloads the photo from LAPL and returns it as a PIL Image object.
+        """
+        r = requests.get(url)
+        obj = BytesIO(response.content)
+        return Image.open(obj)
