4ff2d3682d07aa7204d9c8c2f6995aa14ef561c1
palewire <b@palewi.re>
2022-11-29 09:52:14 -0800

    Clear everything out. Toss in our data

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: loaddb
-
-
-loaddb:
-	heroku pg:backups:capture DATABASE_URL
-	heroku pg:backups:download
-	pg_restore --verbose --clean --no-acl --no-owner -h localhost -d lapl latest.dump
-	rm latest.dump
--- project/urls.py
+++
@@ -1,9 +0,0 @@
-from photos import views
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.photo_list, name="photo-list"),
-]
